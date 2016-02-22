e5067a0301676d05e89902d1e2c04a251798f2e3
Scott Trinh <scott@cn2tech.com>
2016-02-22 10:39:53 -0500

    Add CommonJS module loader plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,22 @@
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
   entry: 'src/shootproof.js',
   sourceMap: true,
   plugins: [
+    nodeResolve({
+      jsnext: true,
+      main: true,
+    }),
+    commonjs({
+      include: ['node_modules/**'],
+      exclude: ['node_modules/lodash-es/**'],
+    }),
     babel(),
   ],
   format: 'cjs',
   moduleName: 'shootproof',
-  dest: 'dist/shootproof.js',
+  dest: 'dist/shootproof.cjs.js',
 };
